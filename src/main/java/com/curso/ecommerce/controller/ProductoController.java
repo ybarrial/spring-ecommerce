9da4b5204e37a2481c09548b98b8d9514dd5f8db
Your Name <yefferson.barrial@avivel.com.pe>
2024-03-14 23:48:39 -0500

    add method create in controller

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -13,4 +13,9 @@ public class ProductoController {
         return "productos/show";
     }
 
+    @GetMapping("/create")
+    public String create() {
+        return "productos/create";
+    }
+
 }
