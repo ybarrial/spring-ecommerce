6ed7b1f26d3825afa3cfa1056acaecb94f256b26
Your Name <yefferson.barrial@avivel.com.pe>
2024-03-15 19:10:12 -0500

    implement method save Producto

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -1,12 +1,23 @@
 package com.curso.ecommerce.controller;
 
+import com.curso.ecommerce.model.Producto;
+import com.curso.ecommerce.model.Usuario;
+import com.curso.ecommerce.service.ProductoService;
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+
+    @Autowired
+    private ProductoService productoService;
 
     @GetMapping("")
     public String show() {
@@ -16,6 +27,15 @@ public class ProductoController {
     @GetMapping("/create")
     public String create() {
         return "productos/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Producto producto) {
+        LOGGER.info("Este es el objeto producto {}", producto);
+        Usuario u = new Usuario(1,"","","","","","","");
+        producto.setUsuario(u);
+        productoService.save(producto);
+        return "redirect:/productos";
     }
 
 }
