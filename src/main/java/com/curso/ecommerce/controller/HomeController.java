d3eef4cf6a87db46d87b485ce412baf15df14d0e
Your Name <yefferson.barrial@avivel.com.pe>
2024-03-17 14:48:05 -0500

    functionality delete product of carrito

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -75,4 +75,29 @@ public class HomeController {
         return "usuario/carrito";
     }
 
+    // quitar un producto del carrito
+    @GetMapping("/delete/cart/{id}")
+    public String deleteProductoCart(@PathVariable Integer id, Model model) {
+        List<DetalleOrden> ordenesNueva = new ArrayList<DetalleOrden>();
+
+        for (DetalleOrden detalleOrden: detalles) {
+            if (detalleOrden.getProducto().getId() != id) {
+                ordenesNueva.add(detalleOrden);
+            }
+        }
+
+        //poner la nueva lista con los productos restantes a la eliminacion
+        detalles = ordenesNueva;
+
+        double sumaTotal = 0;
+        sumaTotal = detalles.stream().mapToDouble(dto -> dto.getTotal()).sum();
+
+        orden.setTotal(sumaTotal);
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
+
+
+        return "usuario/carrito";
+    }
+
 }
