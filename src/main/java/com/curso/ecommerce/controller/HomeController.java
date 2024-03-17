c0d38755c69eb5868462aa7c3eb468959e4520d4
Your Name <yefferson.barrial@avivel.com.pe>
2024-03-17 15:01:43 -0500

    validation when product is the same in add cart & see product in carrito view option

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -64,7 +64,13 @@ public class HomeController {
         detalleOrden.setTotal(producto.getPrecio() * cantidad);
         detalleOrden.setProducto(producto);
 
-        detalles.add(detalleOrden);
+        //validar que el producto no se anada 2 veces
+        Integer idProducto = producto.getId();
+        boolean ingresado = detalles.stream().anyMatch( p -> p.getProducto().getId() == idProducto );
+
+        if (!ingresado) {
+            detalles.add(detalleOrden);
+        }
 
         sumaTotal = detalles.stream().mapToDouble(dto -> dto.getTotal()).sum();
 
@@ -96,8 +102,15 @@ public class HomeController {
         model.addAttribute("cart", detalles);
         model.addAttribute("orden", orden);
 
-
         return "usuario/carrito";
+    }
+
+    @GetMapping("/getCart")
+    public String getCart(Model model) {
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
+
+        return "/usuario/carrito";
     }
 
 }
