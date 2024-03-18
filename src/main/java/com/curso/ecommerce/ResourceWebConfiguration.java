c771bf6d7997ed352e9c19b610d2c90c4927200a
Your Name <yefferson.barrial@avivel.com.pe>
2024-03-17 22:43:48 -0500

    detail

--- src/main/java/com/curso/ecommerce/ResourceWebConfiguration.java
+++ src/main/java/com/curso/ecommerce/ResourceWebConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ResourceWebConfiguration implements WebMvcConfigurer {
 
+    // CLASE QUE PERMITE APUNTAR HACIA LOS RECURSOS DE LAS IMAGENES PARA OBTENERLOS DE MANERA RELATIVA SOLO PAUNTANDO A ESA URL(/images/**)
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**").addResourceLocations("file:images/");
