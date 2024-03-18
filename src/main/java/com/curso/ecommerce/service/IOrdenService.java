090116c0d333896209fdab0709db2afe0c324c14
Your Name <yefferson.barrial@avivel.com.pe>
2024-03-18 18:14:48 -0500

    function to generate number of order

--- src/main/java/com/curso/ecommerce/service/IOrdenService.java
+++ src/main/java/com/curso/ecommerce/service/IOrdenService.java
@@ -2,7 +2,11 @@ package com.curso.ecommerce.service;
 
 import com.curso.ecommerce.model.Orden;
 
+import java.util.List;
+
 public interface IOrdenService {
+
+    List<Orden> findAll();
 
     Orden save(Orden orden);
 
