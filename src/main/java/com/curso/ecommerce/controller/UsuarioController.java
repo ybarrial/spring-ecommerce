8c2812b5aab477ed7bc69e7888dbe9647fb9790a
Your Name <yefferson.barrial@avivel.com.pe>
2024-03-19 17:29:44 -0500

    show usuario register view

---
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -0,0 +1,24 @@
+package com.curso.ecommerce.controller;
+
+import com.curso.ecommerce.service.IUsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("usuario")
+public class UsuarioController {
+
+    @Autowired
+    private IUsuarioService usuarioService;
+
+
+    @GetMapping("/registro")
+    public String create() {
+        return "/usuario/registro";
+    }
+
+
+
+}
