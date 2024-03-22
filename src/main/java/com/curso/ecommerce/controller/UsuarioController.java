c6894fb7b8cfbe00ebbb638e016bb66572cb8621
Your Name <yefferson.barrial@avivel.com.pe>
2024-03-22 14:01:28 -0500

    add register user view

--- src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -1,9 +1,13 @@
 package com.curso.ecommerce.controller;
 
+import com.curso.ecommerce.model.Usuario;
 import com.curso.ecommerce.service.IUsuarioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,12 +17,21 @@ public class UsuarioController {
     @Autowired
     private IUsuarioService usuarioService;
 
+    private final Logger logger = LoggerFactory.getLogger(UsuarioController.class);
+
 
     @GetMapping("/registro")
     public String create() {
         return "/usuario/registro";
     }
 
+    @PostMapping("/save")
+    public String save(Usuario usuario) {
+        logger.info("Usuario registro: {}", usuario);
+        usuario.setTipo("USER");
+        usuarioService.save(usuario);
+        return "redirect:/";
+    }
 
 
 }
